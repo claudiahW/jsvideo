d10ff49e7d375561340656fc7643dd83fcdb00a1
claudiahW <murungiclaud@gmail.com>
2024-01-11 19:07:30 +0300

    add pause and play functions

--- scripts.js
+++ scripts.js
@@ -11,9 +11,11 @@ const video = document.querySelector(".video-container");
 
 btn.addEventListener("click", function(){
 if(!btn.classList.contains("slide")){
-    btn.classList.add("slide")
+    btn.classList.add("slide");
+    video.pause();
 }
 else{
- btn.classList.remove("slide");   
+ btn.classList.remove("slide"); 
+ video.play();  
 }
-})
+});
